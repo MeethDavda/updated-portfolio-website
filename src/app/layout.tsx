c40c70500a9ecfa26b5bc32b0ab9647c777c7cd3
MeethDavda <mjdavda02@gmail.com>
2025-02-16 00:46:34 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} min-h-screen font-[family-name:var(--font-geist-sans)] antialiased max-w-4xl mx-auto py-12 sm:py-24`}
       >
         {children}
       </body>
